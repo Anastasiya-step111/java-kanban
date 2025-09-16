800c0798ea5ce2b4dc1370ff90d59c4f593c8d9c
Anastasiya Stepanova <anastasiya.step111@yandex.ru>
2025-09-16 21:17:57 +0500

    Исправить код соответственно CodeStyle

--- src/ru/practicum/manager/TaskManager.java
+++ src/ru/practicum/manager/TaskManager.java
@@ -12,26 +12,43 @@ public interface TaskManager {
         public <T extends Task> void update(T task);
 
         List<Task> getAllTasks();
+
         Task getTaskById(int id);
+
         Task createTask(Task task);
+
         void updateTask(Task task);
+
         void deleteTask(int id);
+
         void deleteAllTasks();
 
         List<Epic> getAllEpics();
+
         Epic getEpicById(int id);
+
         Epic createEpic(Epic epic);
+
         void updateEpic(Epic epic);
+
         void deleteEpic(int id);
+
         void deleteAllEpics();
 
         List<Subtask> getAllSubtasks();
+
         Subtask getSubtaskById(int id);
+
         List<Subtask> getSubtasksByEpicId(int id);
+
         Subtask createSubtask(Subtask subtask);
+
         void deleteAllSubtasks();
+
         void removeSubtaskById(int id);
+
         void updateSubtask(Subtask subtask, int id);
+
         List<Task> getHistory();
 
         HistoryManager getHistoryManager();
