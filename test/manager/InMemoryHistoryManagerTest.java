cc46f00689bed3784da9adb1abc416e4198822db
Anastasiya Stepanova <anastasiya.step111@yandex.ru>
2025-10-19 22:43:12 +0500

    Внести изменения в тесты в связи с добавлением полей времени

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -10,6 +10,8 @@ import ru.practicum.model.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,12 +32,28 @@ class InMemoryHistoryManagerTest {
     Subtask subtask2;
     Subtask subtask3;
 
+    LocalDateTime startTime1 = LocalDateTime.of(2026, 3, 1, 8, 0);
+    Duration duration1 = Duration.ofHours(1);
+
+    LocalDateTime startTime2 = LocalDateTime.of(2026, 3, 3, 9, 0);
+    Duration duration2 = Duration.ofHours(2);
+
+    LocalDateTime startTime3 = LocalDateTime.of(2026, 3, 5, 11, 0);
+    Duration duration3 = Duration.ofHours(1);
+
+    LocalDateTime startTime4 = LocalDateTime.of(2026, 3, 7, 12, 0);
+    Duration duration4 = Duration.ofHours(1);
+
+    LocalDateTime startTime5 = LocalDateTime.of(2026, 3, 9, 13, 0);
+    Duration duration5 = Duration.ofHours(2);
+
 
     @BeforeEach
     public void beforeEach() {
-        task1 = manager.createTask(new Task("Купить продукты", "Хлеб яйца масло", manager, Status.NEW));
+        task1 = manager.createTask(new Task("Купить продукты", "Хлеб яйца масло", manager, Status.NEW,
+                startTime1, duration1));
         task2 = manager.createTask(new Task("Постирать вещи", "Разделить по цветам", manager,
-                Status.NEW));
+                Status.NEW, startTime2, duration2));
 
         epic1 = manager.createEpic(new Epic("Учить английский", "Очень страшная задача", manager,
                 Status.NEW));
@@ -44,11 +62,12 @@ class InMemoryHistoryManagerTest {
 
 
         subtask1 = manager.createSubtask(new Subtask("Найти репетитора", "Почитать отзывы", manager,
-                epic1.getId(), Status.NEW));
+                epic1.getId(), Status.NEW, startTime3, duration3));
         subtask2 = manager.createSubtask(new Subtask("Разбираться в IDEA",
-                "Переводить все встреченные слова", manager, epic1.getId(), Status.NEW));
+                "Переводить все встреченные слова", manager, epic1.getId(), Status.NEW, startTime4,
+                duration4));
         subtask3 = manager.createSubtask(new Subtask("Изучить вопрос на драйв2",
-                "Сделать список вариантов", manager, epic2.getId(), Status.NEW));
+                "Сделать список вариантов", manager, epic2.getId(), Status.NEW, startTime5, duration5));
     }
     @Test
     void testAddTaskAndGetHistory() {
